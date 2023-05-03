a3ae0f25b7d300e7cb5d2126f34b95b24c225745
anuragdw0710 <anuragdw0710@gmail.com>
2023-05-04 04:00:08 +0530

    First commit

---
+++ config/dbConfig.js
@@ -0,0 +1,18 @@
+const { error } = require('console');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URL);
+
+const connection = mongoose.connection;
+
+connection.on('connected',()=>{
+    console.log('MongoDB connection is successful');
+});
+
+connection.on('error',()=>{
+    console.log('Error in MongoDB connection', error);
+});
+
+module.exports =mongoose;
+
+
